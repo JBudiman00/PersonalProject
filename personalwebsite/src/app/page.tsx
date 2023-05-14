f2365a88c5403c7ce698d869f20bd11e50b22e3d
JBudiman00 <n8nbudiman@gmail.com>
2023-05-14 10:06:25 -0500

    Added NavBar

---
+++ personalwebsite/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="grid h-screen text-center text-[#FDE2F3] text-2xl">
+      <div className="my-60">
+        <p>Hi, my name is Jonathan Budiman.</p>
+        <p>I’m an aspiring full-stack developer</p></div>
+      </div>
+  )
+}
---
+++ personalwebsite/src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+export default function Test () {
+    return (
+        <>
+        <div className="h-6"></div>
+        <div className="grid grid-cols-3 h-16">
+            <div className="col-start-2 col-span-1 text-2xl text-[#2A2F4F] bg-[#E5BEEC] text-center rounded-lg align-middle">
+                <p>Projects</p>
+            </div>
+        </div>
+        </>
+    );
+}
